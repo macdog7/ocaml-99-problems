30915f2af28e2472163668ec86594698025e8abd
macdog <137728258+macdog7@users.noreply.github.com>
2023-11-13 14:29:08 -0700

    problem seven

---
+++ seven/bin/main.ml
@@ -0,0 +1,22 @@
+type 'a node =
+        | One of 'a
+        | Many of 'a node list;;
+
+let flatten list =
+    let rec aux acc = function
+      | [] -> acc
+      | One x :: t -> aux (x :: acc) t
+      | Many l :: t -> aux (aux acc l) t in
+    List.rev (aux [] list);;
+
+let result = flatten [One "a"; Many [One "b"; Many [One "c" ;One "d"]; One "e"]];;
+
+let rec print_arr arr = 
+        match arr with 
+        | [] -> print_endline ""
+        | first :: rest -> print_endline first; print_arr rest
+;;
+
+let _ = print_arr result;;
+
+
